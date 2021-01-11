add63f4a92423c0e3cf204a6517db7eff328fba8
Wilson <karaewilson16@gmail.com>
2021-01-11 15:07:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
 destructivelyRemoveElementFromBeginningOfArray(chocolateBars)
 
 function removeElementFromBeginningOfArray(chocolateBars) {
-    chocolateBars.slice(1)
+    chocolateBars = chocolateBars.slice(1)
     return chocolateBars
 }
 
