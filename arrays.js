298a36f722527b519ce910b23123934bdf98eb59
Wilson <karaewilson16@gmail.com>
2021-01-11 15:03:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars) {
+  return (['foo', ...chocolateBars])
+}
+addElementToBeginningOfArray('foo')
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars) {
+  chocolateBars.unshift('foo')
+  return chocolateBars
+}
+destructivelyAddElementToBeginningOfArray('foo', chocolateBars)
+
+function destructivelyAddElementToEndOfArray(chocolateBars) {
+  chocolateBars.push('foo')
+  return chocolateBars
+}
+
+destructivelyAddElementToEndOfArray(chocolateBars, 'foo')
+
+function addElementToEndOfArray(chocolateBars) {
+  return ([...chocolateBars, "foo"])
+}
+
+addElementToEndOfArray('foo')
+
+function accessElementInArray(chocolateBars) {
+  return (chocolateBars, 3)
+}
+
+accessElementInArray(chocolateBars)
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  chocolateBars.shift()
+  return chocolateBars
+}
+
+destructivelyRemoveElementFromBeginningOfArray(chocolateBars)
+
+function removeElementFromBeginningOfArray(chocolateBars) {
+    chocolateBars.slice(1)
+    return chocolateBars
+}
+
+removeElementFromBeginningOfArray(chocolateBars)
